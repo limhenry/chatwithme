680a7249b802ae7735dbb92bcb269ed32d805d50
limhenry <henry71896@gmail.com>
2015-11-12 20:58:44 +0800

    Update

--- main.js
+++ main.js
@@ -1,62 +1,56 @@
 (function(document) {
-    'use strict';
+'use strict';
 
-    var app = document.querySelector('#app');
-    var profilePic = "http://usn.com.np/wp-content/uploads/2014/07/dummy-avatar.png";
-    var profileName = "Anonymous";
-    app.items = [];
+	var app = document.querySelector('#app');
+	var profilePic = "http://usn.com.np/wp-content/uploads/2014/07/dummy-avatar.png";
+	var profileName = "Anonymous";
 
-    app.firebaseURL = 'https://chat-with-me-2183.firebaseio.com';
-    app.firebaseProvider = 'google';
+	app.items = [];
+	app.firebaseURL = 'https://chat-with-me-2183.firebaseio.com';
+	app.firebaseProvider = 'google';
 
+	app.updateItems = function(snapshot) {
+		this.items = [];
+		snapshot.forEach(function(childSnapshot) {
+			var item = childSnapshot.val();
+			var UTCTime = new Date(item.time);
+			item.time = UTCTime.getHours() + ":" + (UTCTime.getMinutes() < 10?'0':'') + UTCTime.getMinutes();
+			var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June","July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+			item.date = UTCTime.getUTCDate() + " " + monthNames[UTCTime.getUTCMonth()] + " " + UTCTime.getUTCFullYear();
+			this.push('items', item);
+		}.bind(this));
+		setTimeout(scrollToBottom, 0);
+		function scrollToBottom(){
+			document.getElementById('mainContainer').scrollTop = 999999999999999999999999;
+		}      
+	};
 
-    app.updateItems = function(snapshot) {
-      this.items = [];
-      console.log(snapshot);
-      snapshot.forEach(function(childSnapshot) {
-        var item = childSnapshot.val();
-        console.log(item);
-        this.push('items', item);
-      }.bind(this));
-        setTimeout(scrollToBottom, 0);
-        function scrollToBottom(){
-            document.getElementById('mainContainer').scrollTop = 99999999999999999999999999999999999;
-        }      
-    };
+	app.addItem = function(event) {
+		event.preventDefault(); // Don't send the form!
+		if (app.newItemValue.length > 0){
+			var currentTime = (new Date()).getTime();
+			this.ref.push({
+				text: app.newItemValue,
+				img: profilePic,
+				username: profileName,
+				time: currentTime
+			});
+			app.newItemValue = '';        
+		}
+	};
 
+	app.onFirebaseError = function(event) {
+		this.$.errorToast.text = event.detail.message;
+		this.$.errorToast.show();
+	};
 
-    app.addItem = function(event) {
-      event.preventDefault(); // Don't send the form!
-      this.ref.push({
-        text: app.newItemValue,
-        img: profilePic,
-        username: profileName
-      });
-      app.newItemValue = '';
-    };
-
-    app.toggleItem = function(event) {
-      this.ref.
-        child(event.model.item.uid).
-        update({done: event.model.item.done});
-    };
-
-    app.deleteItem = function(event) {
-      this.ref.child(event.model.item.uid).remove();
-    };
-
-    app.onFirebaseError = function(event) {
-      this.$.errorToast.text = event.detail.message;
-      this.$.errorToast.show();
-    };
-
-    app.onFirebaseLogin = function(e) {
-        profilePic = e.detail.user.google.profileImageURL;
-        profileName = e.detail.user.google.displayName;
-        this.ref = new Firebase(this.firebaseURL);
-        this.ref.on('value', function(snapshot) {
-          app.updateItems(snapshot);
-        });
-    };
+	app.onFirebaseLogin = function(e) {
+		profilePic = e.detail.user.google.profileImageURL;
+		profileName = e.detail.user.google.displayName;
+		this.ref = new Firebase(this.firebaseURL);
+		this.ref.on('value', function(snapshot) {
+			app.updateItems(snapshot);
+		});
+	};
 
 })(document);
